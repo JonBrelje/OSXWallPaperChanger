27bbfcd9e6195e755f5447d448a8f9f0092c46a9
JonBrelje <jonathan.brelje@gmail.com>
2017-01-27 20:35:23 -0500

    Python actually changes background with time now

--- change_desktop_background.sh
+++ change_desktop_background.sh
@@ -1,13 +1,16 @@
+#!/bin/sh
+
 # First argument must be absolute file path to desired image
 image_path=$1
-
+echo $
+echo $1
 #Call apple script to change the background image of each desktop
 osascript << EOF
 tell application "System Events"
 set desktopCount to count of desktops
 repeat with desktopNumber from 1 to desktopCount
 tell desktop desktopNumber
-set picture to POSIX file "/Users/JonBrelje/Pictures/alcatraz.jpg"
+set picture to POSIX file "$1"
 end tell
 end repeat
 end tell
--- choose_background.py
+++ choose_background.py
@@ -5,8 +5,12 @@ time = datetime.datetime.now()
 hour = time.hour
 
 if hour <= 8:
-	subprocess.call(["""./change_desktop_background.sh '/Users/JonBrelje/Pictures/Grand Tetons.jpg'"""])
+	subprocess.call(['./change_desktop_background.sh', '/Users/JonBrelje/Pictures/Background Photos/Grand Tetons.jpg'])
+elif hour <= 12:
+	subprocess.call(['./change_desktop_background.sh', '/Users/JonBrelje/Pictures/Background Photos/great smoky mountains.jpg'])
 elif hour <= 16:
-	subprocess.call(["""./change_desktop_background.sh '/Users/JonBrelje/Pictures/Mt. Hood.jpg'"""])
+	subprocess.call(['./change_desktop_background.sh', '/Users/JonBrelje/Pictures/Background Photos/Mt. Hood.jpg'])
+elif hour <= 20:
+	subprocess.call(['./change_desktop_background.sh', '/Users/JonBrelje/Pictures/Background Photos/mt hood snow.jpg'])
 else:
-	subprocess.call(["""./change_desktop_background.sh '/Users/JonBrelje/Pictures/Mt. Hood at Dusk.jpg'"""])
+	subprocess.call(['./change_desktop_background.sh', '/Users/JonBrelje/Pictures/Background Photos/Mt. Hood at Dusk.jpg'])
